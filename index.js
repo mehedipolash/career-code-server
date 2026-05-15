d219fb76755201ac33025d691c4efdd7a2110a82
mehedipolash <mehedipolash228@gmail.com>
2026-05-16 01:21:19 +0600

    ready for deployment

--- index.js
+++ index.js
@@ -1,3 +1,290 @@
+// const express = require("express");
+// const cors = require("cors");
+// const app = express();
+// const jwt = require("jsonwebtoken");
+// const cookieParser = require("cookie-parser");
+// const port = process.env.PORT || 3000;
+// require("dotenv").config();
+
+
+// // middleware
+// app.use(
+//   cors({
+//     origin: ["http://localhost:5173"],
+//     credentials: true,
+//   }),
+// );
+
+// app.use(express.json());
+// app.use(cookieParser());
+
+
+// //our custom middleware
+// const logger = (req, res, next) => {
+//   console.log("inside the logger middleware");
+//   next();
+// };
+
+
+// const verifyToken = (req,res,next)=>{
+//   const token = req?.cookies?.token;
+//   // check token existance
+//    if (!token) {
+//     return res.status(401).send({ message: "unauthorized access" });
+//   }
+
+//   // verify token
+//   jwt.verify(token, process.env.JWT_ACCESS_SECRET, (err, decoded) => {
+//     if (err) {
+//       return res.status(401).send({ message: "unauthorized access" });
+//     }
+//     req.decoded = decoded;
+//     next();
+//   });
+// }
+
+
+
+// //---------------------------------
+
+// const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.azj20jt.mongodb.net/?appName=Cluster0`;
+
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   },
+// });
+
+// async function run() {
+//   try {
+//     // await client.connect();
+
+//     // FIXED DB NAME
+//     const jobCollection = client.db("carrerCode").collection("jobs");
+
+//     const applicationCollection = client
+//       .db("carrerCode")
+//       .collection("applications");
+
+//     // jwt token related api
+//     app.post("/jwt", async (req, res) => {
+//       const userInfo = req.body;
+
+//       const token = jwt.sign(userInfo, process.env.JWT_ACCESS_SECRET, {
+//         expiresIn: "2h",
+//       });
+
+//       res.cookie("token", token, {
+//         httpOnly: true,
+//         secure: false,
+//       });
+
+//       res.send({ success: true });
+//     });
+
+//     //------ jobs related apis -------//
+
+//     // only can get all users jobs post not individuals.
+
+//     /* app.get("/jobs", async (req, res) => {
+//       const result = await jobCollection.find().toArray();
+//       res.send(result);
+//     }); */
+
+//     // after modify (you can get all jobs or specific users jobs through query from url)
+
+//     app.get("/jobs", async (req, res) => {
+//       const email = req.query.email;
+//       const query = {};
+//       if (email) {
+//         query.hr_email = email;
+//       }
+
+//       const result = await jobCollection.find(query).toArray();
+//       res.send(result);
+//     });
+
+//     //could be done but should not be done.
+//     // app.get("/jobsByEmailAddress", async (req, res) => {
+//     //   const email = req.query.email;
+//     //   const query = { hr_email: email };
+//     //   const result = await jobCollection.find(query).toArray();
+//     //   res.send(result);
+//     // });
+
+//     //show the no of application deposited for each job posted by specific recruiter (in mongodb you can access any collection to other collection)
+
+//     app.get("/jobs/applications",verifyToken, async (req, res) => {
+//       const email = req.query.email;
+//       const query = { hr_email: email };
+//       const jobs = await jobCollection.find(query).toArray();
+
+//       // should use aggregate to have uptimum query
+
+//       for (job of jobs) {
+//         const applicationQuery = { jobId: job._id.toString() };
+//         const application_count =
+//           await applicationCollection.countDocuments(applicationQuery);
+//         job.application_count = application_count;
+//       }
+//       res.send(jobs);
+//     });
+
+//     app.get("/jobs/:id", async (req, res) => {
+//       const id = req.params.id;
+//       const query = { _id: new ObjectId(id) };
+//       const result = await jobCollection.findOne(query);
+//       res.send(result);
+//     });
+
+//     app.post("/jobs", async (req, res) => {
+//       const newJob = req.body;
+//       const result = await jobCollection.insertOne(newJob);
+//       res.send(result);
+//     });
+
+//     //---   application for job related apis---//
+
+//     //previous get api
+
+//     /* (app.get("/applications", async (req, res) => {
+//       const email = req.query.email;
+//       const query = {
+//         applicantEmail: email,
+//       };
+//       const result = await applicationCollection.find(query).toArray();
+//       res.send(result);
+//     });) */
+
+//     //to get all users applications or individuals
+//     /* app.get("/applications", async (req, res) => {
+//       const email = req.query.email;
+
+//       const query = {
+//         applicantEmail: email,
+//       };
+
+//       const result = await applicationCollection.find(query).toArray();
+
+//       // bad way to aggregate data
+
+//       for (const application of result) {
+//         const jobId = application.jobId;
+
+//         const jobquery = {
+//           _id: new ObjectId(jobId),
+//         };
+
+//         const job = await jobCollection.findOne(jobquery);
+
+//         application.company = job.company;
+//         application.title = job.title;
+//         application.company_logo = job.company_logo;
+//       }
+
+//       res.send(result);
+//     }); */
+
+//     app.get("/applications", logger,verifyToken, async (req, res) => {
+//       const email = req.query.email;
+
+//       const query = {};
+
+//       if (email) {
+//         query.applicantEmail = email;
+//       }
+
+//       const result = await applicationCollection.find(query).toArray();
+
+//       // enrich application data with job info
+//       for (const application of result) {
+//         const jobId = application.jobId;
+
+//         const jobquery = {
+//           _id: new ObjectId(jobId),
+//         };
+
+//         const job = await jobCollection.findOne(jobquery);
+
+//         if (job) {
+//           application.company = job.company;
+//           application.title = job.title;
+//           application.company_logo = job.company_logo;
+//         }
+//       }
+
+//       res.send(result);
+//     });
+//     //delete application by user
+//     app.delete("/applications/:id", async (req, res) => {
+//       const id = req.params.id;
+
+//       const query = {
+//         _id: new ObjectId(id),
+//       };
+
+//       const result = await applicationCollection.deleteOne(query);
+
+//       res.send(result);
+//     });
+
+//     // get applicant details for each job posted by recruiter
+
+//     /*  app.get('/applications/:id',async(req,res)=>{
+//             not this way!
+//     }) */
+
+//     app.get("/applications/job/:job_id", async (req, res) => {
+//       const job_id = req.params.job_id;
+//       console.log(job_id);
+//       const query = { jobId: job_id };
+//       const result = await applicationCollection.find(query).toArray();
+//       res.send(result);
+//     });
+
+//     app.post("/applications", async (req, res) => {
+//       const application = req.body;
+//       const result = await applicationCollection.insertOne(application);
+//       res.send(result);
+//     });
+
+//     // update the status of application by recruiter(initially there was no element like status in applicants table)
+//     app.patch("/applications/:id", async (req, res) => {
+//       const id = req.params.id;
+//       const filter = { _id: new ObjectId(id) };
+//       const updatedDoc = {
+//         $set: {
+//           status: req.body.status,
+//         },
+//       };
+//       const result = await applicationCollection.updateOne(filter, updatedDoc);
+//       res.send(result);
+//     });
+
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Connected to MongoDB!");
+//   } finally {
+//   }
+// }
+
+// run().catch(console.dir);
+
+// app.get("/", (req, res) => {
+//   res.send("Career code is Running...");
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server running on port ${port}`);
+// });
+
+
+// -------------------  updated from claude ---------------------------//
+
+
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -6,30 +293,33 @@ const cookieParser = require("cookie-parser");
 const port = process.env.PORT || 3000;
 require("dotenv").config();
 
+// --------------------------------
+//          MIDDLEWARE
+// --------------------------------
 
-// middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173"], // update after client deploys
     credentials: true,
-  }),
+  })
 );
 
 app.use(express.json());
 app.use(cookieParser());
 
-
-//our custom middleware
+// custom logger middleware
 const logger = (req, res, next) => {
   console.log("inside the logger middleware");
   next();
 };
+app.use(logger);
 
-
-const verifyToken = (req,res,next)=>{
+// verify token middleware
+const verifyToken = (req, res, next) => {
   const token = req?.cookies?.token;
-  // check token existance
-   if (!token) {
+
+  // check token existence
+  if (!token) {
     return res.status(401).send({ message: "unauthorized access" });
   }
 
@@ -41,11 +331,11 @@ const verifyToken = (req,res,next)=>{
     req.decoded = decoded;
     next();
   });
-}
+};
 
-
-
-//---------------------------------
+// --------------------------------
+//         DATABASE SETUP
+// --------------------------------
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
@@ -61,16 +351,17 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
 
-    // FIXED DB NAME
     const jobCollection = client.db("carrerCode").collection("jobs");
-
     const applicationCollection = client
       .db("carrerCode")
       .collection("applications");
 
-    // jwt token related api
+    // --------------------------------
+    //       JWT TOKEN RELATED API
+    // --------------------------------
+
     app.post("/jwt", async (req, res) => {
       const userInfo = req.body;
 
@@ -80,50 +371,42 @@ async function run() {
 
       res.cookie("token", token, {
         httpOnly: true,
-        secure: false,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
       });
 
       res.send({ success: true });
     });
 
-    //------ jobs related apis -------//
+    app.post("/logout", (req, res) => {
+      res.clearCookie("token", {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      });
+      res.send({ success: true });
+    });
 
-    // only can get all users jobs post not individuals.
+    // --------------------------------
+    //       JOBS RELATED APIS
+    // --------------------------------
 
-    /* app.get("/jobs", async (req, res) => {
-      const result = await jobCollection.find().toArray();
-      res.send(result);
-    }); */
-
-    // after modify (you can get all jobs or specific users jobs through query from url)
-
+    // get all jobs or specific user's jobs by email query
     app.get("/jobs", async (req, res) => {
       const email = req.query.email;
       const query = {};
       if (email) {
         query.hr_email = email;
       }
-
       const result = await jobCollection.find(query).toArray();
       res.send(result);
     });
 
-    //could be done but should not be done.
-    // app.get("/jobsByEmailAddress", async (req, res) => {
-    //   const email = req.query.email;
-    //   const query = { hr_email: email };
-    //   const result = await jobCollection.find(query).toArray();
-    //   res.send(result);
-    // });
-
-    //show the no of application deposited for each job posted by specific recruiter (in mongodb you can access any collection to other collection)
-
-    app.get("/jobs/applications",verifyToken, async (req, res) => {
+    // get jobs with application count for each job posted by recruiter
+    app.get("/jobs/applications", verifyToken, async (req, res) => {
       const email = req.query.email;
       const query = { hr_email: email };
       const jobs = await jobCollection.find(query).toArray();
-
-      // should use aggregate to have uptimum query
 
       for (job of jobs) {
         const applicationQuery = { jobId: job._id.toString() };
@@ -134,6 +417,7 @@ async function run() {
       res.send(jobs);
     });
 
+    // get single job by id
     app.get("/jobs/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
@@ -141,59 +425,22 @@ async function run() {
       res.send(result);
     });
 
+    // post a new job
     app.post("/jobs", async (req, res) => {
       const newJob = req.body;
       const result = await jobCollection.insertOne(newJob);
       res.send(result);
     });
 
-    //---   application for job related apis---//
+    // --------------------------------
+    //     APPLICATION RELATED APIS
+    // --------------------------------
 
-    //previous get api
-
-    /* (app.get("/applications", async (req, res) => {
-      const email = req.query.email;
-      const query = {
-        applicantEmail: email,
-      };
-      const result = await applicationCollection.find(query).toArray();
-      res.send(result);
-    });) */
-
-    //to get all users applications or individuals
-    /* app.get("/applications", async (req, res) => {
-      const email = req.query.email;
-
-      const query = {
-        applicantEmail: email,
-      };
-
-      const result = await applicationCollection.find(query).toArray();
-
-      // bad way to aggregate data
-
-      for (const application of result) {
-        const jobId = application.jobId;
-
-        const jobquery = {
-          _id: new ObjectId(jobId),
-        };
-
-        const job = await jobCollection.findOne(jobquery);
-
-        application.company = job.company;
-        application.title = job.title;
-        application.company_logo = job.company_logo;
-      }
-
-      res.send(result);
-    }); */
-
-    app.get("/applications", logger,verifyToken, async (req, res) => {
+    // get all applications or specific user's applications by email query
+    app.get("/applications", verifyToken, async (req, res) => {
       const email = req.query.email;
 
       const query = {};
-
       if (email) {
         query.applicantEmail = email;
       }
@@ -203,11 +450,7 @@ async function run() {
       // enrich application data with job info
       for (const application of result) {
         const jobId = application.jobId;
-
-        const jobquery = {
-          _id: new ObjectId(jobId),
-        };
-
+        const jobquery = { _id: new ObjectId(jobId) };
         const job = await jobCollection.findOne(jobquery);
 
         if (job) {
@@ -219,25 +462,16 @@ async function run() {
 
       res.send(result);
     });
-    //delete application by user
+
+    // delete application by user
     app.delete("/applications/:id", async (req, res) => {
       const id = req.params.id;
-
-      const query = {
-        _id: new ObjectId(id),
-      };
-
+      const query = { _id: new ObjectId(id) };
       const result = await applicationCollection.deleteOne(query);
-
       res.send(result);
     });
 
     // get applicant details for each job posted by recruiter
-
-    /*  app.get('/applications/:id',async(req,res)=>{
-            not this way!
-    }) */
-
     app.get("/applications/job/:job_id", async (req, res) => {
       const job_id = req.params.job_id;
       console.log(job_id);
@@ -246,13 +480,14 @@ async function run() {
       res.send(result);
     });
 
+    // submit a new application
     app.post("/applications", async (req, res) => {
       const application = req.body;
       const result = await applicationCollection.insertOne(application);
       res.send(result);
     });
 
-    // update the status of application by recruiter(initially there was no element like status in applicants table)
+    // update application status by recruiter
     app.patch("/applications/:id", async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
@@ -272,6 +507,10 @@ async function run() {
 }
 
 run().catch(console.dir);
+
+// --------------------------------
+//         SERVER START
+// --------------------------------
 
 app.get("/", (req, res) => {
   res.send("Career code is Running...");
